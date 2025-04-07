aeb4d575ac3caf62fe58d2851db6d5e979b44e62
Mandla Moyo <mandla@noredink.com>
2025-04-07 03:58:27 -0700

    Fix attempt for prod issue

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig(({ mode }) => {
     },
     define: {
       'process.env.NODE_ENV': JSON.stringify(mode),
+      'process.platform': JSON.stringify('browser'),
     },
   }
 });
